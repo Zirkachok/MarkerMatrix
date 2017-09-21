46ac19146f76d3cafab9b61a75a0b7f9fbccf501
Julien Beaudaux <j.beaudaux@fde.fr>
2017-09-21 15:29:45 +0200

    Finished RTM export
    Signed-off-by: Julien Beaudaux <j.beaudaux@fde.fr>

--- VisibleMarker.py
+++ VisibleMarker.py
@@ -20,6 +20,7 @@ import docxManager
 from lxml import etree
 from clint.textui import colored, puts
 
+import xlwt
 from xlwt import Workbook
 
 
@@ -90,14 +91,20 @@ def hierarchy_mode():
 
 	markDict = {}
 
+	docList = []
+	phaseDict = {}
+	systemDict = {}
+
 	# List nodes and their markers list
 	tree = etree.parse("hierarchy.xml")
 	for phase in tree.xpath("/Model/Phase"):
+		phaseDict[phase.get("ID")] = phase.get("name")
 
 		for system in phase:
 			# Single-document system
 			if len(system) == 1:
 				tmpID = phase.get("ID") + "." + system[0].get("ID")
+				docList.append((tmpID, system[0].get("name"), ""))
 
 				for elem in system[0]:
 					if elem.tag == "Path":
@@ -110,6 +117,7 @@ def hierarchy_mode():
 
 			else:
 				tmpID = phase.get("ID") + "." + system.get("ID")
+				systemDict[tmpID] = system.get("name")
 
 				tmpSysMark = []
 
@@ -118,6 +126,7 @@ def hierarchy_mode():
 						pass
 					elif elem.tag == "Doc":
 						tmpID = phase.get("ID") + "." +elem.get("ID")
+						docList.append((tmpID, elem.get("name"), phase.get("ID") + "." + system.get("ID")))
 
 						for detail in elem:
 							if detail.tag == "Path":
@@ -197,38 +206,129 @@ def hierarchy_mode():
 
 	print colored.green("\nTraceability matrix Successfully created")
 
-	create_RTM(markDict)
+	create_RTM(markDict, phaseDict, systemDict, docList)
 
 
-def create_RTM(markersDict):
+def create_RTM(markersDict, phases, systems, docs):
 	lst = []
 
-	# os.remove("VisibleMatrix-RTM.xls")
+	phasesDone = []
 
 	book = Workbook()
 	
 	matrix = book.add_sheet('Traceability Matrix')
 
-	i=1
-	for ids in markersDict:
-		lst = list(set(lst).union(markersDict[ids]))
-		matrix.write(0, i, ids)
-		i+=1
+	defaultStyle = xlwt.easyxf('alignment: horizontal center, vertical center')
+	phaseStyle = 'font: height 200, bold 1; alignment: horizontal center, vertical center; borders: left thick, right thick; pattern: pattern solid, fore_color '
 
-	i=1
-	for mark in lst:
-		matrix.write(i, 0, mark)
-		i+=1
+	colors = ["white", "light_green", "orange", "yellow", "pale_blue", "blue"]
 
-	# ligne1 = feuil1.row(1)
-	# ligne1.write(0,'1')
-	# ligne1.write(1,'235.0')
-	# ligne1.write(2,'424.0')
-	# ligne1.write(3,'a')
+	matrix.col(0).width = 5000
 
-	matrix.col(0).width = 10000
+	curPhase = ""
+	startMerge = 0
+	endMerge = 0
+
+	curSys = ""
+	startSys = 0
+	endSys = 0
+
+	markers = []
+
+	i = 1
+	colIndex = 0
+	curStyle = xlwt.easyxf(phaseStyle + colors[colIndex])
+
+	matrix.write(0, 0, "Phase", curStyle)
+	matrix.write(1, 0, "System", curStyle)
+	matrix.write(2, 0, "Document", curStyle)
+
+	for ids in docs:
+		tmpId, tmpName, tmpSys = ids
+
+		for mark in markersDict[tmpId]:
+			if mark not in markers:
+				markers.append(mark)
+
+		if (tmpId.split(".")[0]) not in phasesDone:
+			curStyle = xlwt.easyxf(phaseStyle + colors[colIndex])
+
+			if startMerge != 0 and endMerge != 0:
+				matrix.write_merge(0, 0, startMerge, endMerge, curPhase, curStyle)
+
+			startMerge = endMerge + 1
+			curPhase = phases[(tmpId.split(".")[0])]
+
+			phasesDone.append(tmpId.split(".")[0])
+			colIndex += 1
+			curStyle = xlwt.easyxf(phaseStyle + colors[colIndex])
+
+
+		if tmpSys == "":
+			matrix.write(1, i, "", curStyle)
+			if curSys != "":
+				matrix.write_merge(1, 1, startSys, endSys, systems[curSys], curStyle)
+				curSys = tmpSys
+				startSys = i
+		else:
+			if tmpSys != curSys:
+				if curSys != "":
+					matrix.write_merge(1, 1, startSys, endSys, systems[curSys], curStyle)
+				
+				curSys = tmpSys
+				startSys = i
+
+			endSys = i
+
+
+		matrix.write(2, i, tmpName, curStyle)
+		matrix.col(i).width = 10000
+		i += 1
+		endMerge += 1
+
+	matrix.write_merge(0, 0, startMerge, endMerge, curPhase, curStyle)
+
+	i = 3
+	for mark in markers:
+		matrix.write(i, 0, mark, defaultStyle)
+
+		j = 0
+		for ids in docs:
+			tmpId, tmpName, tmpSys = ids
+
+			j += 1
+			if mark in markersDict[tmpId]:
+				matrix.write(i, j, "X", defaultStyle)
+
+
+		i += 1
+
+	# font: height 400, bold 1, color red, italic 1, underline single;
+	# alignment: horizontal center, vertical center;
+	# borders: left thin, right thin, top thick, bottom thick, top_color green;
+	# pattern: pattern solid, fore_color yellow'
+	# num_format_str = 'YYYY-MM-DD'
 
 	book.save("VisibleMatrix-RTM.xls")
+
+def check_marker_unicity(path, model):
+	print colored.yellow("Check unicity of markers %s in file %s\n"%(model, path))
+
+	_, markList = extract_markers(path)
+
+	missMark = []
+	uniq = [x for i, x in enumerate(markList) if x not in markList[0:i]]
+	for item in uniq:
+		if item[0:len(model)+1] == ("[" + model):
+			if markList.count(item) > 1:
+				missMark.append((item, markList.count(item)))
+
+	if len(missMark) == 0:
+		print colored.green("No redundant markers found in document")
+	else:
+		print colored.red("Redundant markers present in document:")
+		for item, times in missMark:
+			print colored.red("\tMarker %s redundant (%d times)"%(item, times))
 
 
 if __name__ == '__main__':
@@ -240,13 +340,19 @@ if __name__ == '__main__':
 	elif len(sys.argv) >= 2 and (sys.argv[1] == "--model" or sys.argv[1] == "-m"):
 		show_version("Model-based full testing mode")
 		hierarchy_mode()
+	elif len(sys.argv) == 4 and (sys.argv[1] == "--unicity" or sys.argv[1] == "-u"):
+		show_version("Marker unicity testing mode")
+		check_marker_unicity(sys.argv[2], sys.argv[3])
 	else:
-		print "USAGE : %s [-c FILE1 ... FILEn] [-h]"%(sys.argv[0])
+		print "USAGE : %s [-c FILE1 ... FILEn] [-m] [-u FILE MARK]"%(sys.argv[0])
 
 		print "Comparison mode"
 		print "\t--compare, -c : compare files provided in arguments"
 
 		print "Automated mode"
 		print "\t--model, -m : fetches the doc model and tests compliance"
+
+		print "Marker unicity mode"
+		print "\t--unicity, -u : fetches the doc model and tests compliance"
 
 		sys.exit(-1)
